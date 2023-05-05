36f90ead0d94a5fe7fe0818fd2f0f06720f5379f
Noah Hoang <hoincbusiness@gmail.com>
2023-05-04 20:59:49 -0700

    Added get one product feature

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -31,6 +31,27 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "product_name", "price", "stock", "category_id"],
+    include: [
+      {
+        model: Category,
+        attributes: ["id", "category_name"],
+      },
+      {
+        model: Tag,
+        attributes: ["id", "tag_name"],
+      },
+    ],
+  })
+    .then((dbProductData) => res.json(dbProductData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // create new product
